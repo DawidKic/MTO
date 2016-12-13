8f6cf9ba68d38e8837e3852ae75bba2ed43016c0
Dawid Kicinski <d.kicinski12@gmail.com>
2016-12-13 22:24:06 +0100

    Stworzenie pierwszego testu: Dodawanie dodatnich

--- KalkulatorTDD/src/test/java/test/AddTest.java
+++ KalkulatorTDD/src/test/java/test/AddTest.java
@@ -1,11 +1,16 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class AddTest {
+	Calculator calculator = new Calculator();
+	int sum = calculator.add(1,3);
+	int testSum = 5;
 	@Test
 	public void SumTest(){
-		
+		System.out.println("@Test add positive: " + sum + " = " + testSum);
+		assertEquals(sum, testSum);
 	}
 
 }
