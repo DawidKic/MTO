a52df48596c81de12f6cf71089e2f7366c87ebc7
Dawid Kicinski <d.kicinski12@gmail.com>
2016-12-13 22:46:38 +0100

    Dodanie kalkulatora na podstawie testow

---
+++ KalkulatorTDD/src/main/java/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class Calculator {
+	//dodawanie
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+}
--- KalkulatorTDD/src/test/java/test/AddTest.java
+++ KalkulatorTDD/src/test/java/test/AddTest.java
@@ -1,16 +1,38 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
+import calculator.Calculator;
 
 public class AddTest {
-	Calculator calculator = new Calculator();
-	int sum = calculator.add(1,3);
-	int testSum = 5;
+	Calculator calculator;
+	@Before
+	public void Create()
+	{
+		calculator = new Calculator();
+	}
+	
 	@Test
-	public void SumTest(){
-		System.out.println("@Test add positive: " + sum + " = " + testSum);
-		assertEquals(sum, testSum);
+	public void AddTestPositive(){
+		int testSum = 4;
+		System.out.println("@Test add positive");
+		assertEquals(testSum, calculator.add(1,3));
+	}
+	
+	@Test
+	public void SumTestOneNegative(){
+		int testSum = -2;
+		System.out.println("@Test add one negative");
+		assertEquals(testSum, calculator.add(1,-3));
+	}
+	
+	@Test
+	public void SumTestTwoNegatives(){
+		int testSum = -4;
+		System.out.println("@Test add one negative");
+		assertEquals(testSum, calculator.add(-1,-3));
 	}
 
 }
